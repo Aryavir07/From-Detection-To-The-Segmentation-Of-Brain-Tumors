9a9360e41f3e6ffa48d7ce2e5537cd7d266058f0
Aryavir07 <aryavir.singh07@gmail.com>
2022-06-05 14:15:00 +0530

    New Changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from plot_mri import plot_scan
 secret_key = secrets.token_hex(16)
 
 app = Flask(__name__)
-
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 UPLOAD_FOLDER = 'static/uploads/'
  
 app.secret_key = secret_key
@@ -31,25 +31,24 @@ def get_prediction(filename):
         path = [f"./static/uploads/{filename}"]
         obj = Prediction(path, model, model_seg)
         result = obj.make_prediction()
-        df = pd.DataFrame([result])
-        df.columns = ["image_path", "predicted_mask", "has_mask"]
-        if(df.shape[0] == 0):
-            return False
-        plot_scan(df)
-        return True
+        if(result[2] == 0):
+            return [] , False
+        return result, True
     
- 
-@app.route('/')
-def home():
+def clean_dir():
     files = glob.glob("static/uploads/*")
     for f in files:
         os.remove(f)
     files = glob.glob("static/predicted/*")
     for f in files:
         os.remove(f)
+ 
+@app.route('/')
+def home():
+    clean_dir()
     return render_template('index.html')
  
-@app.route('/predict/', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_image():
     if 'file' not in request.files:
         flash('No file part')
@@ -63,21 +62,32 @@ def upload_image():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        result = get_prediction(filename)
-        if(result):
+        result, detected = get_prediction(filename)
+        if(detected):
+            df = pd.DataFrame([result])
+            df.columns = ["image_path", "predicted_mask", "has_mask"]
+            plot_scan(df)
             return render_template('predict.html', filename=filename)
         else:
+            clean_dir()
             flash("Hurray! No Tumor Detected")
             return render_template('index.html')
     else:
         flash('Allowed image types are - tif, png, jpg, jpeg, gif')
         return redirect(request.url)
         
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
  
 @app.route('/display/<filename>')
 def display_image(filename):
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
  
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     app.run()
